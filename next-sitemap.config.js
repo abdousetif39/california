f5ab3c2bb9f002d6ac485fb70a742501c0f90748
abdousetif39 <abdousetif940@gmail.com>
2026-03-07 17:53:40 +0100

    update cookie system

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,20 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://californiataxcalculators.com',
-  generateRobotsTxt: true, // سيقوم بتوليد ملف robots.txt تلقائياً
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/', // السماح لعناكب البحث بالوصول لكل الموقع
-      },
-    ],
-    // إضافة مسار خريطة الموقع داخل robots.txt
-    additionalSitemaps: [
-      'https://californiataxcalculators.com/sitemap.xml',
-    ],
-  },
-  exclude: ['/404'], // استبعاد صفحات الأخطاء من خريطة الموقع
+  generateRobotsTxt: true, // ستقوم المكتبة تلقائياً بربط الخريطة داخل robots.txt
+  exclude: ['/404'], // استبعاد صفحات الأخطاء
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
