717590038146d18a3acb4dc901fdaea633ab5c5a
abdousetif39 <abdousetif940@gmail.com>
2026-03-23 22:00:16 +0100

    Update: AdSense ready + content improvements

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,7 +30,7 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       {/* Google Consent Mode */}
-      <Script id="google-consent-mode" strategy="afterInteractive">
+      <Script id="google-consent-mode" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
