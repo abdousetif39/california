b32f20ef37fa6d3b471319c9d3892ee1e0f2e265
abdousetif39 <abdousetif940@gmail.com>
2026-03-23 23:05:08 +0100

    Update: AdSense ready + content improvements

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
 
-        
+        <meta httpEquiv="Content-Security-Policy" content="
+default-src 'self' https:;
+script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://pagead2.googlesyndication.com https://fundingchoicesmessages.google.com;
+style-src 'self' 'unsafe-inline' https:;
+img-src 'self' data: https:;
+connect-src 'self' https:;
+frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com;
+">
 
         {/* ✅ Google Consent Mode */}
         <script
